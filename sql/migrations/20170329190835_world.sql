7bcb560921b161640515330d5afd2fe95fbfd669
eir <m-laniakea@users.noreply.github.com>
2017-03-29 19:11:38 -0700

    Scripts/Blackwing Lair: Add Nefarius' Scepter Emotes

---
+++ sql/migrations/20170329190835_world.sql
@@ -0,0 +1,13 @@
+INSERT INTO `migrations` VALUES ('20170329190835');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+(-1469031,  'Looking for this: Red Scepter Shard? Come and get it...',    NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  'Nefarius\' yell at start of Red Scepter Shard Run'),
+(-1469038,  'I wonder, heroes, what will you do when I manage to destroy this shard? Will the world be beyond Salvation?',  NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  'Nefarius\' Scepter Taunt '),
+(-1469039,  'Lord Victor Nefarius\'s laughter echoes through the halls of Blackwing.',  NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  3,  0,  11, 'Lord Victor Nefarius\'s laughter echoes through the halls of Blackwing.'),
+(-1469040,  'What\'s this? A chip in the shard? Finally, a weakness exposed... It won\'t be long now.', NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  ''),
+(-1469041,  'Would it not be glorious if you somehow managed to defeat me, but not before I could destroy the only hope Kalimdor may have? I am giddy with anticipation. Soon... Soon it will be dust.',    NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  NULL),
+(-1469042,  'Not even my father could have planned such tragedy... such chaos. You will never make it! Time is running out!',   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  NULL),
+(-1469043,  'Ah, the protective layer is nearly gone. Do you hear that, heroes? That... That is the sound of inevitability. Of your hopes and dreams being crushed under the weight of my being.',  NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  NULL),
+(-1469044,  'Lord Victor Nefarius\'s laughter can once more be heard through the halls of Blackwing.',  NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  3,  0,  11, NULL),
+(-1469045,  'The scepter shard is no more! Your beloved Kalimdor shall soon be a desolate wasteland. I will, of course, turn the Eastern Kingdoms into an ashen, lifeless void...', NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   NULL,   0,  1,  0,  0,  'Nefarius\' Final Say on Scepter Run fail');
+
